e50ae49fc6c626bcd12d9df99ef0d3494cf577ce
Alexander White <alexzonder@e1.ru>
2019-01-06 10:29:01 +0000

    uploading a project

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,42 @@
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+
+class AdminSiteTests(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='admin@e1.ru',
+            password='password123'
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email='test@e1.ru',
+            password='password123',
+            name='Test user full name'
+        )
+
+    def test_users_listed(self):
+        """ Test that users atre listed on user page """
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)
+
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
+
+    def test_user_chane_page(self):
+        """ Test that the user edit page works okay
+            in our case it should return code 200 """
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        """ Tests that the creating user page works """
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
---
+++ app/core/tests/test_models.py
@@ -0,0 +1,39 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_successful(self):
+        """ Test that a new user with an e-mail is successfuly created """
+        email = "test@e1.ru"
+        password = "testpassword123456"
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """ Test the email for a new user is normalized """
+        email = 'test@E1.RU'
+        user = get_user_model().objects.create_user(email, 'test123')
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """ Test that creating a user with no email goes to error """
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test123')
+
+    def test_create_new_superuser(self):
+        """ Test creating a new superuser """
+        user = get_user_model().objects.create_superuser(
+            'supertest@e1.ru',
+            "test123321"
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
